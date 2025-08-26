e2d4aa022be21679f5be297364d0e71679e37c52
Lazy Grapefruit <decoupled456@hotmail.com>
2025-08-26 10:26:45 -0700

    fix name and version

--- src/lib/addon.js
+++ src/lib/addon.js
@@ -2,13 +2,14 @@
 // @ts-nocheck
 import { addonBuilder } from "stremio-addon-sdk";
 import { streamHandler } from "./addon-handlers";
+import { name, version } from "../../package.json";
 
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
     id: "community.byoiap",
-    name: "byoiap",
+    name: name,
     description: "Bring Your Own Indexer and Provider",
-    version: "0.0.1",
+    version: version,
 
     catalogs: [],
     resources: ["stream"],
